b806664f8bfc9fd0a956e17b46ad8b47e9001a78
AnnaK <anna_morozova_kz@list.ru>
2023-03-23 19:10:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int account = 100; // начальный счет клиента (руб)
+        int amount = 1100; // сумма пополнения (руб)
+        int bonus;
+
+        if (amount > 1000) {
+            bonus = amount / 100;
+        } else {
+            bonus = 0;
+        }
+
+        System.out.println("Ваш итоговый счет: " + (account + amount + bonus));
+        System.out.println("Начислено бонусов : " + bonus);
+    }
+}
